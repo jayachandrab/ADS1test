85b060635da5a5193e1e4bcb72e9fa70fdde0315
Jayachandra <jayachandra1805@gmail.com>
2018-10-01 16:22:45 +0530

    version1

---
+++ Bank.java
@@ -0,0 +1,59 @@
+package com.bank;
+
+class Account{
+	double balance;
+
+	public Account(double balance) {
+		if(balance<0.0) {
+			System.out.println("initial balance invalid");
+		}
+		else {
+			this.balance = balance;
+		}
+		
+	}
+	
+	public void creditAccount(double balance) {
+		
+	}
+	
+     public void debitAccount(double balance) {
+		
+	}
+     
+     public double getBalance() {
+    	 return this.balance;
+     }
+	
+	
+	
+}
+
+class SavingsAccount extends Account{
+   
+	float interest;
+	
+	public SavingsAccount(double balance,int interest) {
+		
+		// TODO Auto-generated constructor stub
+		super(balance);
+		this.interest=interest;
+	}
+	
+	public double calculateSavingsInterest() {
+		return 0.0;
+	}
+	
+}
+
+class CheckingAccount{
+	
+}
+public class Bank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
